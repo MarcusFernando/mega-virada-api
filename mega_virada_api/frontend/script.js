7d7368c43a06c8df370f3907360ac054c5db7b31
MarcusFernando <163361991+MarcusFernando@users.noreply.github.com>
2026-01-02 12:00:28 -0300

    Correção Mobile e Redirect

--- mega_virada_api/frontend/script.js
+++ mega_virada_api/frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "";
 
 function openTab(tabName) {
     document.querySelectorAll('.tab-content').forEach(el => el.classList.remove('active'));
